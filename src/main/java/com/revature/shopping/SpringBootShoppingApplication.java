74b16e21dfd8d1a50d71c40fc851c8da943466bf
ramirosot0 <sotoramiro010@gmail.com>
2022-07-13 02:17:05 -0700

    first commit

---
+++ src/main/java/com/revature/shopping/SpringBootShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.revature.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/shopping/SpringBootShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
